1eae9597aa81b41ddfdc547d2bd05b2373d98e73
sonoma <wllamsmth1@gmail.com>
2024-04-07 19:53:24 +0300

    Commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 # Создаем нейронную сеть
 net = network.Network([784, 30, 10])
 
+# Выводим количество нейронов в каждом слое
 for i in range(net.num_layers):
     print('Количество нейронов в слое', i + 1, ':', net.sizes[i])  # выводим количество нейронов в каждом слое
 
@@ -34,6 +35,3 @@ for i in range(net.num_layers - 1):
     print(np.round(net.weights[i], 2))
     print('\nBias', i + 1, ':')
     print(np.round(net.biases[i], 2))
-
-# Обучаем нейронную сеть
-net.sgd(training_data, 30, 10, 3.0, test_data=test_data)
